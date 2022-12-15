b74d35c6a5c0d67f18616653ad04b3ceda9824ef
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2022-12-16 00:41:20 +0300

    Fix some setup issues

--- setup.py
+++ setup.py
@@ -1,26 +1,20 @@
-import os
-import sys
 import setuptools
-
-sys.path.append(os.path.abspath(__file__))
-from theme import __version__, __author__, __author_email__
-
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="theme",
-    version=__version__,
-    author=__author__,
-    author_email=__author_email__,
+    version='0.1.0',
+    author='Ilia Moiseev',
+    author_email='ilia.moiseev.5@yandex.ru',
     license='MIT',
     description="Simple CLI labeling tool for text classification",
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Operating System :: Linux"
+        "Operating System :: POSIX :: Linux"
     ],
     package_dir={"theme": "./theme"},
     packages=setuptools.find_packages(),
