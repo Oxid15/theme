e8a4348165667b403f04e36d012bb8bc366b0ab3
Ilia Moiseev <villeman.5@yandex.ru>
2024-06-27 18:01:37 +0300

    Update example

--- example.py
+++ example.py
@@ -1,23 +1,22 @@
 from theme import Theme
 
 
-id2label = {
-    '0': 'fake',
-    '1': 'real'
-}
+id2label = {"0": "fake", "1": "real"}
 
 t = Theme(
     id2label=id2label,
-    unmarked_table='data.csv',
-    marked_table='markup.csv',
-    id_col='Unnamed: 0',
-    text_col='text',
-    label_col='label',
-    show_cols=['title'],
+    unmarked_table="theme/data.csv",
+    marked_table="markup.csv",
+    id_col="Unnamed: 0",
+    text_col="text",
+    label_col="label",
+    show_cols=["title"],
     select_label=None,
     write_meta=True,
-    meta_prefix={'labeling_goal': 'Generate initial dataset'},
-    cache_skipped=True
+    meta_prefix={"labeling_goal": "Generate initial dataset"},
+    cache_skipped=True,
+    label_session_minutes=2,
+    break_minutes=1,
 )
 
 t.run()
